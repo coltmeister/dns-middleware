b952455cfc1aae32bb949b91ebff9b8d87a7c243
Craig McCown <craigrmccown@gmail.com>
2017-03-01 14:20:37 -0500

    writing response back to client

--- vpn/vpn.go
+++ vpn/vpn.go
@@ -13,6 +13,18 @@ func (vpn Vpn) Name() string {
 }
 
 func (vpn Vpn) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+    /* Get current request state */
     state := request.Request{W: w, Req: r}
+
+    /* Create response message */
+    a := &dns.Msg{}
+    a.SetReply(r)
+    a.Compress = true
+    a.Authoritative = true
+
+    /* Set response bits and write response to client */
+    state.SizeAndDo(a)
+    w.WriteMsg(a)
+
     return 0, nil
 }
